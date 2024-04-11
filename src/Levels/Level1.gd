c542a577aff1c457125b4d3ae59aecddf7eaef31
Alton Dupre <altondupre@eatel.net>
2024-04-11 14:41:58 -0500

    Added objective markers and a level complete screen with score and ranking, updated level select to have a synopsis.

--- src/Levels/Level1.gd
+++ src/Levels/Level1.gd
@@ -1,10 +1,55 @@
 extends Node3D
 
 var items_dropped = 0
+var tasks = []
+@export var highscore:int = 1
+@export var lowscore:int = 6
 
 func _ready():
 	rotation.y = deg_to_rad(-45)
+	for task in $Tasks.get_children():
+		tasks.push_back(task)
+	tasks[0].show()
+
+func _process(delta):
+	if Input.is_action_just_pressed("start"):
+		get_parent().changeState("res://src/testScenes/levelbuttons.tscn")
+	
+	if !$UI/Score/Suspense.is_stopped():
+		$UI/Score.text = str("Items Dropped: ", (randi()%100) + 1)
 
 func _on_object_hit_ground(object):
 	items_dropped += 1
-	$Score.text = str("Items Dropped: ", items_dropped)
+
+func _on_task_body_entered(body):
+	if body.is_in_group("player") && $TaskWait.is_stopped():
+		tasks[0].hide()
+		tasks[0].disconnect("body_entered",Callable(self,"_on_task_body_entered"))
+		tasks.pop_front()
+		if tasks.size() == 0: _on_level_complete()
+		else: 
+			tasks[0].show()
+		$TaskWait.start()
+
+func _on_level_complete():
+	$UI.show()
+	$UI/CompleteAnim.play("appear")
+
+func calculate_score():
+	$UI/Score.text = str("Items Dropped: ", items_dropped)
+	$UI/Score/Suspense2.start()
+
+func _on_complete_anim_animation_finished(anim_name):
+	$UI/Score.show()
+	$UI/Score/Suspense.start()
+
+func set_score():
+	$UI/HBoxContainer/Star1.show()
+	if items_dropped <= lowscore:
+		$UI/HBoxContainer/Star2.show()
+		if items_dropped <= highscore:
+			$UI/HBoxContainer/Star3.show()
+	$UI/Button.show()
+
+func _on_button_pressed():
+	get_parent().changeState("res://src/testScenes/levelbuttons.tscn")
--- src/testScenes/sceneLoader.gd
+++ src/testScenes/sceneLoader.gd
@@ -1,10 +1,8 @@
-extends Control
+extends VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,4 +15,3 @@ func _on_pressed(num):
 			get_tree().root.get_child(0).changeState("res://src/Levels/level_1.tscn")
 		1:
 			get_tree().root.get_child(0).changeState()
-
